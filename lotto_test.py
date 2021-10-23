d636d5cdf92ca0b500f3cb02ff801141bfc8a64a
Eoin <L00170151@student.lyit.ie>
2021-10-23 12:13:19 +0100

    initial commit to push to new repo

---
+++ lotto_test.py
@@ -0,0 +1,22 @@
+"""
+#
+File    :lotto_test.py
+Created :21/10/2021
+Author  : Eoin O'Mahony
+"""
+
+
+import pytest
+from lotto_example import Lotto
+
+
+def test_lotto():
+    x = Lotto
+    assert x.lotto(Lotto, 21) == "Passed"
+
+
+def test_lotto_negative():
+    x = Lotto
+    assert x.lotto(Lotto, 9) == "Failed"
+    print("Successful")
+
